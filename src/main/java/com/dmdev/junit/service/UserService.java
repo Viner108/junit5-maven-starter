48440bb96df2c86c713ef32dffa609d92da5ceb4
Viner108 <andrukovichl@mail.ru>
2024-02-15 14:26:01 +0300

    7-7 AssertJ & Hamcrest

--- src/main/java/com/dmdev/junit/service/UserService.java
+++ src/main/java/com/dmdev/junit/service/UserService.java
@@ -2,10 +2,10 @@ package com.dmdev.junit.service;
 
 import com.dmdev.junit.dto.User;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.toMap;
 
 public class UserService {
     private final List<User> users=new ArrayList<>();
@@ -13,8 +13,8 @@ public class UserService {
         return users;
     }
 
-    public boolean add(User user) {
-        return users.add(user);
+    public void add(User... users) {
+        this.users.addAll(Arrays.asList(users));
     }
 
     public Optional<User> login(String username, String password) {
@@ -22,5 +22,10 @@ public class UserService {
                 .filter(user -> user.getUsername().equals(username))
                 .filter(user -> user.getPassword().equals(password))
                 .findFirst();
+    }
+
+    public Map<Integer, User> getAllConvertedById() {
+        return users.stream()
+                .collect(toMap(User::getId, Function.identity()));
     }
 }
--- src/test/java/com/dmdev/junit/service/UserServiceTest.java
+++ src/test/java/com/dmdev/junit/service/UserServiceTest.java
@@ -1,75 +1,105 @@
 package com.dmdev.junit.service;
 
 import com.dmdev.junit.dto.User;
+import org.hamcrest.collection.IsEmptyCollection;
+import org.hamcrest.collection.IsMapContaining;
 import org.junit.jupiter.api.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.hamcrest.MatcherAssert;
+import org.assertj.core.api.Assertions;
+
+import static org.hamcrest.collection.IsEmptyCollection.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserServiceTest {
     private UserService userService;
-    private static final User IVAN=User.of(1, "Ivan", "123");
-    private static final User PETR=User.of(2, "Petr", "111");
+    private static final User IVAN = User.of(1, "Ivan", "123");
+    private static final User PETR = User.of(2, "Petr", "111");
+
     @BeforeAll
-     void init(){
-        System.out.println("Before all: "+this);
+    void init() {
+        System.out.println("Before all: " + this);
     }
+
     @BeforeEach
-    void prepare(){
-        System.out.println("Before each: "+this);
-        userService=new UserService();
+    void prepare() {
+        System.out.println("Before each: " + this);
+        userService = new UserService();
     }
+
     @Test
-    void userEmptyIfNoUserAdded(){
-        System.out.println("Test 1: "+this);
+    void userEmptyIfNoUserAdded() {
+        System.out.println("Test 1: " + this);
         List<User> all = userService.getAll();
         assertTrue(all.isEmpty());
     }
-    @Test
-    void loginSuccessIfUserExists(){
-        userService.add(IVAN);
-        Optional<User> maybeUser = userService.login(IVAN.getUsername(),IVAN.getPassword());
-
-        assertTrue(maybeUser.isPresent());
-        maybeUser.ifPresent(user -> assertEquals(IVAN,user));
-    }
-    @Test
-    void loginFailIfPasswordIsNotCorrect(){
-        userService.add(IVAN);
-
-        Optional<User> maybeUser = userService.login(IVAN.getUsername(),"sfsv");
-
-        assertTrue(maybeUser.isEmpty());
-    }
 
     @Test
-    void loginFailIfUserDoesNotExist(){
-        userService.add(IVAN);
-
-        Optional<User> maybeUser = userService.login("ghfjdk",IVAN.getPassword());
-
-        assertTrue(maybeUser.isEmpty());
-    }
-
-
-    @Test
-    void usersSizeIfUserAdded(){
-        System.out.println("Test 2: "+this);
+    void usersSizeIfUserAdded() {
+        System.out.println("Test 2: " + this);
         userService.add(IVAN);
         userService.add(PETR);
 
-        List<User> users= userService.getAll();
-        assertEquals(2,users.size());
+        List<User> users = userService.getAll();
+
+        MatcherAssert.assertThat(users, empty());
+        Assertions.assertThat(users).hasSize(2);
+//        assertEquals(2,users.size());
     }
+
+    @Test
+    void loginSuccessIfUserExists() {
+        userService.add(IVAN);
+        Optional<User> maybeUser = userService.login(IVAN.getUsername(), IVAN.getPassword());
+
+        assertTrue(maybeUser.isPresent());
+        maybeUser.ifPresent(user -> Assertions.assertThat(user).isEqualTo(IVAN));
+//        maybeUser.ifPresent(user -> assertEquals(IVAN,user));
+    }
+
+    @Test
+    void usersConvertedToMapById() {
+        userService.add(IVAN, PETR);
+
+        Map<Integer, User> users = userService.getAllConvertedById();
+
+        MatcherAssert.assertThat(users, IsMapContaining.hasKey(IVAN.getId()));
+        assertAll(
+                () -> Assertions.assertThat(users).containsKeys(IVAN.getId(), PETR.getId()),
+                () -> Assertions.assertThat(users).containsValues(IVAN, PETR)
+        );
+    }
+
+    @Test
+    void loginFailIfPasswordIsNotCorrect() {
+        userService.add(IVAN);
+
+        Optional<User> maybeUser = userService.login(IVAN.getUsername(), "sfsv");
+
+        assertTrue(maybeUser.isEmpty());
+    }
+
+    @Test
+    void loginFailIfUserDoesNotExist() {
+        userService.add(IVAN);
+
+        Optional<User> maybeUser = userService.login("ghfjdk", IVAN.getPassword());
+
+        assertTrue(maybeUser.isEmpty());
+    }
+
     @AfterEach
-    void deleteDataFromDatabase(){
-        System.out.println("After each: "+this);
+    void deleteDataFromDatabase() {
+        System.out.println("After each: " + this);
     }
+
     @AfterAll
-     void closeConnectionPool(){
-        System.out.println("After all: "+this);
+    void closeConnectionPool() {
+        System.out.println("After all: " + this);
     }
 }
